5b34989641a18589f59575c87058469838a55f08
mwangi-george <mwangigeorge648@gmail.com>
2025-08-22 16:27:00 +0300

    Updated config.py

--- app/settings/config.py
+++ app/settings/config.py
@@ -4,3 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_URL = os.getenv('DB_URL')
+PROJECT_NAME = os.getenv('PROJECT_NAME')
+PROJECT_DESCRIPTION = os.getenv('PROJECT_DESCRIPTION')
+PROJECT_VERSION = os.getenv('PROJECT_VERSION')
+PROJECT_LICENSE = os.getenv('PROJECT_LICENSE')
