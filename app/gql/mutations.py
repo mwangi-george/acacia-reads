bfa744f54b5724427b34b2ea14373bd8bd56f684
mwangi-george <mwangigeorge648@gmail.com>
2025-08-27 21:46:36 +0300

    Updated documentation for root mutations class

--- app/gql/mutations.py
+++ app/gql/mutations.py
@@ -1,5 +1,6 @@
 from graphene import ObjectType
 
+# Import mutation classes for different domains (Users, Authors, Books, Orders)
 from app.gql.author.mutations import AddAuthor, UpdateAuthor, DeleteAuthor
 from app.gql.book.mutations import AddBook, UpdateBook
 from app.gql.order.mutations import AddOrder, UpdateOrder
@@ -8,21 +9,45 @@ from app.gql.user.mutations import AddUser, UpdateUser, LoginUser, DeleteUser
 
 class Mutation(ObjectType):
     """
-    GraphQL Mutation.
+    Root GraphQL Mutation class.
 
-    Contains all the GraphQL mutation operations for users, books, authors, & orders
+    This class aggregates all available GraphQL mutations for the system,
+    exposing them as mutation fields that clients can execute.
+
+    The mutations are grouped into the following domains:
+
+    - **User Management**: Create, update, authenticate, and delete users.
+    - **Author Management**: Manage author records (add, update, delete).
+    - **Book Management**: Manage book records (add, update).
+    - **Order Management**: Manage book orders (add, update).
+
+    Each mutation is defined as a GraphQL `Field` that maps to a specific
+    mutation class implementation.
     """
-    add_user = AddUser.Field()
-    update_user = UpdateUser.Field()
-    login_user = LoginUser.Field()
-    delete_user = DeleteUser.Field()
 
-    add_author = AddAuthor.Field()
-    update_author = UpdateAuthor.Field()
-    delete_author = DeleteAuthor.Field()
+    # --------------------------
+    # User Management Mutations
+    # --------------------------
+    add_user = AddUser.Field(description="Register a new user in the system")
+    login_user = LoginUser.Field(description="Authenticate an existing user and return a token")
+    update_user = UpdateUser.Field(description="Update details of an existing user")
+    delete_user = DeleteUser.Field(description="Delete an existing user from the system")
 
-    add_book = AddBook.Field()
-    update_book = UpdateBook.Field()
+    # --------------------------
+    # Author Management Mutations
+    # --------------------------
+    add_author = AddAuthor.Field(description="Add a new author record")
+    update_author = UpdateAuthor.Field(description="Update details of an existing author")
+    delete_author = DeleteAuthor.Field(description="Remove an author record from the system")
 
-    add_order = AddOrder.Field()
-    update_order = UpdateOrder.Field()
+    # --------------------------
+    # Book Management Mutations
+    # --------------------------
+    add_book = AddBook.Field(description="Add a new book record")
+    update_book = UpdateBook.Field(description="Update details of an existing book")
+
+    # --------------------------
+    # Order Management Mutations
+    # --------------------------
+    add_order = AddOrder.Field(description="Create a new order for books")
+    update_order = UpdateOrder.Field(description="Update details of an existing order")
